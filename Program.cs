a7eb417de5b1200341ff7d0500a2fa0f9f1ad7da
tarikhkilim <tarikhkilim@gmail.com>
2020-10-19 17:09:56 +0300

    Değişiklik yapıldı gönderildi

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // KENDİ YAPTIĞIM DOSYAYI DEPOYA BAĞLAMA VE ATMA
+        }
+    }
+}
